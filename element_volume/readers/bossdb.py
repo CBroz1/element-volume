3fcc47ec58aa88544ae09bb60e36f455ec4760bc
CBroz1 <CBroz@DataJoint.com>
2023-01-30 15:55:55 -0600

    Add Resolution/Zoom tables

---
+++ element_volume/readers/bossdb.py
@@ -0,0 +1,154 @@
+import os
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Tuple, Union
+
+import numpy as np
+from element_interface.utils import find_full_path
+from intern import array
+from PIL import Image
+
+from .. import volume
+
+
+class BossDBInterface(array):
+    def __init__(
+        self,
+        channel: Union[Tuple, str],
+        session_key: dict = None,
+        resolution_id: str = None,
+        volume_id: str = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(channel=channel, **kwargs)
+        self._session_key = session_key or dict()
+
+        # If not passed resolution or volume IDs, use the following defaults:
+        self._volume_key = dict(
+            volume_id=volume_id or self.collection_name + "/" + self.experiment_name,
+            resolution_id=resolution_id or self.resolution,
+        )
+
+    def _infer_session_dir(self):
+        root_dir = volume.get_vol_root_data_dir()
+        if isinstance(root_dir, Sequence):
+            root_dir = root_dir[0]
+        inferred_dir = (
+            f"{self.collection_name}/{self.experiment_name}/{self.channel_name}/"
+        )
+        os.makedirs(Path(root_dir) / inferred_dir, exist_ok=True)
+        return inferred_dir
+
+    def _import_resolution(self):
+        volume.Resolution.insert1(
+            dict(
+                resolution_id=self.resolution,  # integer 0-6
+                voxel_unit=self.voxel_unit,  # axis order is either ZYX or XYZ
+                voxel_z_size=self.voxel_size[0 if self.axis_order[0] == "Z" else 2],
+                voxel_y_size=self.voxel_size[1],
+                voxel_x_size=self.voxel_size[2 if self.axis_order[0] == "Z" else 0],
+            ),
+            skip_duplicates=True,
+        )
+
+    def _import_volume(self, volume_id: str = None):
+        if volume_id:
+            self._volume_key.update(dict(volume_id=self.volume_id))
+
+        volume.Volume.insert1(
+            dict(
+                **self._session_key,
+                **self._volume_key,
+                z_size=self.shape[0 if self.axis_order[0] == "Z" else 2],
+                y_size=self.shape[1],
+                x_size=self.shape[2 if self.axis_order[0] == "Z" else 0],
+                channel=self.channel_name,
+                url=self.url,
+            ),
+            skip_duplicates=True,
+        )
+
+    def _get_zoom_id(self, xs, ys):
+        _shape = self.shape
+        y_max, x_max = _shape[1:3] if self.axis_order[0] == "Z" else _shape[-2::1]
+        if xs[0] == 0 and ys[0] == 0 and xs[1] == x_max and ys[1] == y_max:
+            return "Full Image"
+        else:
+            zoom_id = f"X{xs[0]}-{xs[1]}_Y{ys[0]}-{ys[1]}"
+            volume.Zoom.insert1(
+                dict(
+                    zoom_id=zoom_id,
+                    first_start=xs[0],
+                    first_end=xs[1],
+                    second_start=ys[0],
+                    second_end=ys[1],
+                ),
+                skip_duplicates=True,
+            )
+            return zoom_id
+
+    def _fetch_slice_data(self, xs, ys, zs):
+        cutout = self.volume_provider.get_cutout(
+            self._channel, self.resolution, xs, ys, zs
+        )
+        if self.axis_order != self.volume_provider.get_axis_order():
+            data: np.ndarray = np.swapaxes(cutout, 0, 2)
+        else:
+            data: np.ndarray = cutout
+        # NOTE: does not collapse slice by dimension like array.__getitem___ for
+        # convenience when loading into volume.Volume.Slice table
+        return data
+
+    def _string_to_slice_key(self, string_key: str) -> Tuple:
+        output = tuple()
+        items = string_key.strip("[]").split(",")
+        for item in items:
+            if ":" in item:
+                start, stop = list(map(int, item.split(":")))
+            else:
+                start = int(item)
+                stop = start + 1
+            output = (*output, slice(start, stop))
+        return output
+
+    def _import_slices(
+        self, slice_key: Tuple[Union[int, slice]], extension: str = ".png"
+    ):
+        xs, ys, zs = self._normalize_key(key=slice_key)
+        data = self._fetch_slice_data(xs, ys, zs)
+        zoom_id = self._get_zoom_id(xs, ys)
+
+        # If dir provided by get_session, use that. Else infer and mkdir
+        session_path = (
+            volume.get_session_directory(self._session_key) or self._infer_session_dir()
+        )
+        file_name = f"Res{self.resolution}_Zoom{zoom_id}_Z%d{extension}"
+        file_path_relative = session_path + file_name
+        file_path_full = str(
+            find_full_path(volume.get_vol_root_data_dir(), session_path) / file_name
+        )
+
+        for z in range(zs[0], zs[1]):
+            # Z is used as absolute reference within dataset
+            # When saving data, 0-indexed based on slices fetched
+            Image.fromarray(data[z - zs[0]]).save(file_path_full % z)
+
+            volume.Volume.Slice.insert1(
+                dict(
+                    **self._volume_key,
+                    id=z,
+                    zoom_id=zoom_id,
+                    file_path=file_path_relative % z,
+                )
+            )
+
+    def download(
+        self,
+        slice_key: Union[Tuple[Union[int, slice]], str],
+        extension: str = ".png",
+    ):
+        if isinstance(slice_key, str):
+            slice_key = self._string_to_slice_key(slice_key)
+        self._import_resolution()
+        self._import_volume()
+        self._import_slices(slice_key, extension)
--- element_volume/volume.py
+++ element_volume/volume.py
@@ -1,13 +1,23 @@
+import importlib
+import inspect
 import logging
+import pathlib
 
 import datajoint as dj
 
 logger = logging.getLogger("datajoint")
 
 schema = dj.Schema()
+_linking_module = None
 
 
-def activate(schema_name: str, create_schema: bool = True, create_tables: bool = True):
+def activate(
+    schema_name: str,
+    *,
+    create_schema: bool = True,
+    create_tables: bool = True,
+    linking_module: str = None,
+):
     """Activate this schema
 
     Args:
@@ -16,22 +26,120 @@ def activate(schema_name: str, create_schema: bool = True, create_tables: bool =
                             does not yet exist.
         create_tables (bool): when True (default), create schema tables in the database
                              if they do not yet exist.
+        linking_module (str): A string containing the module name or module containing
+            the required dependencies to activate the schema.
+
+    Dependencies:
+    Tables:
+        Session: A parent table to Volume
+    Functions:
+        get_vol_root_data_dir: Returns absolute path for root data director(y/ies) with
+            all volumetric data, as a list of string(s).
+        get_session_directory: When given a session key (dict), returns path to
+            volumetric data for that session as a list of strings.
     """
 
+    if isinstance(linking_module, str):
+        linking_module = importlib.import_module(linking_module)
+    assert inspect.ismodule(
+        linking_module
+    ), "The argument 'dependency' must be a module's name or a module"
+
+    global _linking_module
+    _linking_module = linking_module
+
     schema.activate(
-        schema_name, create_schema=create_schema, create_tables=create_tables
+        schema_name,
+        create_schema=create_schema,
+        create_tables=create_tables,
+        add_objects=_linking_module.__dict__,
     )
+
+
+# -------------- Functions required by the elements-ephys  ---------------
+
+
+def get_vol_root_data_dir() -> list:
+    """Fetches absolute data path to ephys data directories.
+
+    The absolute path here is used as a reference for all downstream relative paths used in DataJoint.
+
+    Returns:
+        A list of the absolute path(s) to ephys data directories.
+    """
+    root_directories = _linking_module.get_vol_root_data_dir()
+    if isinstance(root_directories, (str, pathlib.Path)):
+        root_directories = [root_directories]
+
+    return root_directories
+
+
+def get_session_directory(session_key: dict) -> str:
+    """Retrieve the session directory with volumetric data for the given session.
+
+    Args:
+        session_key (dict): A dictionary mapping subject to an entry in the subject
+            table, and session identifier corresponding to a session in the database.
+
+    Returns:
+        A string for the path to the session directory.
+    """
+    return _linking_module.get_session_directory(session_key)
+
+
+@schema
+class Resolution(dj.Lookup):
+    definition = """ # Resolution of stored data
+    resolution_id: varchar(32) # Shorthand for convention. For BossDB, integer value.
+    ---
+    voxel_unit: varchar(16) # e.g., nanometers
+    voxel_z_size: float # size of one z dimension voxel in voxel_units
+    voxel_y_size: float # size of one y dimension voxel in voxel_units
+    voxel_x_size: float # size of one x dimension voxel in voxel_units
+    """
+
+
+@schema
+class Zoom(dj.Lookup):
+    definition = """ # Image cutoffs when taking a subset of a given slice
+    zoom_id: varchar(32) # Shorthand for zoom convention
+    ---
+    first_start: int # Starting voxel in first dimension (X if taking Z slices)
+    first_end=null: int # Ending voxel plus 1 in first dimension
+    second_start: int # Starting voxel in second dimension (Y if taking Z slices)
+    second_end=null: int # Ending voxel plus 1 in second dimension
+    """
+
+    contents = [
+        ("Full Image", 0, None, 0, None),
+    ]
 
 
 @schema
 class Volume(dj.Manual):
-    definition = """
-    -> Session
-    volume_id : int
+    # NOTE: Session added as nullable because data downloaded from BossDB would not be
+    # associated with a session. Should we enforce this association?
+    definition = """ # Dataset of a contiguous volume
+    volume_id : varchar(32) # shorthand for this volume
+    -> Resolution
+    ---
+    -> [nullable] Session
+    z_size: int # total number of voxels in z dimension
+    y_size: int # total number of voxels in y dimension
+    x_size: int # total number of voxels in x dimension
+    slicing_dimension='z': enum('x','y','z') # perspective of slices
+    channel: varchar(64) # data type or modality (e.g., EM, segmentation, etc.)
+    url=null : varchar(255) # dataset URL
     """
 
-    class File(dj.Part):
-        definition = """
-        -> master
-        file_path: varchar(255)  # filepath relative to root data directory
+    class Slice(dj.Part):
+        # NOTE: The table architecture makes sense as part table, but fetching from
+        # BossDB might entail a subset of slices, which doesn't align with the design
+        # goal of always ingesting master/parts at the same time
+        definition = """ # Slice of a given volume
+        -> Volume
+        id: int # Nth voxel in slicing_dimension
+        -> Zoom
+        ---
+        file_path : varchar(255) # filepath relative to root data directory
         """
